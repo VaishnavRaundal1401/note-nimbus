90f92be83279965f9e500a51c32acd42828c7462
Vaishnav Raundal <vaishnavraundal2016@gmail.com>
2024-05-23 23:57:20 +0530

    used useContext hook to display notes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useContext } from 'react'
 
 function Home() {
   return (
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import NoteContext from "./noteContext";
 const NoteState = (props) =>{
+
+    const notesInitial = [
+        {
+          "_id": "6616fc27edf2bcd913c28c51",
+          "user": "660b1bbcc1e5894e0856b168",
+          "title": "greeting",
+          "description": "Good Night",
+          "tag": "Soja Gandu",
+          "date": "2024-04-10T20:52:55.343Z",
+          "__v": 0
+        }
+      ]
+
+      const [notes, setNotes] = useState(notesInitial);
     return(
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
