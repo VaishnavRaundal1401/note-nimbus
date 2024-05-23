f7ae9f8297766d2cf443cd7058e868c9d469e850
Vaishnav Raundal <vaishnavraundal2016@gmail.com>
2024-05-24 00:07:34 +0530

    context v2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { useContext } from 'react'
+import noteContext from '../context/notes/noteContext';
 
 function Home() {
+  const context = useContext(noteContext);
+  const {notes, setNotes} = context;
   return (
     <div>
       <div className="container my-3">
@@ -26,6 +29,9 @@ function Home() {
 
       <div className="container my-3">
         <h1>Your Notes</h1>
+        {notes.map((notes) =>{
+          return notes.title;
+        })}
       </div>
     </div>
   )
